f2433e5ebd7f022505758acc5feeb9366d740a21
Lingjia Liu <gmutoo@gmail.com>
2021-06-11 11:14:14 +1000

    remove self-signed cert from repo

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const https = require('https')
+const express = require('express');
+const https = require('https');
 const fs = require('fs');
-const app = express()
 
-app.use(express.static('src'))
+const app = express();
+
+app.use(express.static('src'));
 
 const options = {
-    key: fs.readFileSync('assets/localhost.key'),
-    cert: fs.readFileSync('assets/localhost.crt')
+  key: fs.readFileSync('assets/cert/localhost.key'),
+  cert: fs.readFileSync('assets/cert/localhost.crt'),
 };
 
-https.createServer(options, app).listen(443)
-
+https.createServer(options, app).listen(443);
