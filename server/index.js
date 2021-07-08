1fcd764773447cdfd1318f4a9f627c64aa80e9a1
Lingjia Liu <lingjia@canva.com>
2021-07-08 22:47:38 +1000

    setup babel

--- server/index.js
+++ server/index.js
@@ -1,50 +1,5 @@
-import express from 'express';
-import https from 'https';
-import fs from 'fs';
-import WebSocket from 'ws';
-import { connectToDevice } from './sp.js';
-
-const app = express();
-
-app.use(express.static('src'));
-
-app.use('/node_modules', express.static('node_modules'));
-
-app.use('/assets', express.static('assets'));
-
-app.use('/ca', express.static('scripts/ca.pem'));
-
-const options = {
-  key: fs.readFileSync('server/cert/localhost.key'),
-  cert: fs.readFileSync('server/cert/localhost.crt'),
-};
-
-const server = https.createServer(options, app).listen(8443);
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', async (ws) => {
-  try {
-    const port = await connectToDevice((data) => {
-      // eslint-disable-next-line no-console
-      console.debug(`EBB: ${data}`);
-      ws.send(data);
-    });
-    ws.on('message', (message) => {
-      // eslint-disable-next-line no-console
-      console.debug(`Client: ${message}`);
-      port.write(message);
-    });
-    ws.on('close', () => {
-      // eslint-disable-next-line no-console
-      console.debug('Disconnect from client, close EBB');
-      port.write('R\r');
-      port.close();
-    });
-    ws.send('!connected');
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.debug('Failed to connect EBB');
-    ws.close(3000, e.toString());
-  }
-});
+// this entry file enable babel transform to load es modules with modern javascript syntax
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@babel/register');
+// boot the local server
+require('./local.js');
