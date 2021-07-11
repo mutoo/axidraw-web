8d53a4b8983f1f443ad47fe54729c81543fd7452
Lingjia Liu <lingjia@canva.com>
2021-07-12 00:58:48 +1000

    add lan ip to cert

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
-# create cert folder
+
 CERT_DIR=`dirname $0`/../server/cert
+FILENAME_CA_KEY=ca.key
+FILENAME_CA_CERT=ca.pem
+FILENAME_CERT_KEY=localhost.key
+FILENAME_CERT_CSR=localhost.csr
+FILENAME_CERT_EXT=localhost.ext
+FILENAME_CERT_CERT=localhost.crt
+
+# create cert folder
 mkdir -p $CERT_DIR
 cd $CERT_DIR
-# genearte ca key
-openssl genrsa -out ca.key 2048
-# create ca root cert
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.pem
-# generate cert key
-openssl genrsa -out localhost.key 2048
+
+if [ ! -f "$FILENAME_CA_CERT" ]; then
+  # generate ca key
+  openssl genrsa -out $FILENAME_CA_KEY 2048
+  # create ca root cert
+  openssl req -x509 -new -nodes -key $FILENAME_CA_KEY -sha256 -days 365 -out $FILENAME_CA_CERT
+fi
+
+if [ ! -f "$FILENAME_CERT_KEY" ]; then
+  # generate cert key
+  openssl genrsa -out $FILENAME_CERT_KEY 2048
+fi
+
 # create csr
-openssl req -new -nodes -key localhost.key -out localhost.csr
+openssl req -new -nodes -key $FILENAME_CERT_KEY -out $FILENAME_CERT_CSR
 # create ext config
-> localhost.ext cat <<-EOF
+> $FILENAME_CERT_EXT cat <<-EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -21,8 +36,10 @@ subjectAltName = @alt_names
 DNS.1 = localhost
 DNS.2 = $(hostname)
 DNS.3 = $(hostname).local
+DNS.4 = $(ipconfig getifaddr en0)
 EOF
 # signed cert
-openssl x509 -req -in localhost.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out localhost.crt -days 365 -sha256 -extfile localhost.ext
+openssl x509 -req -in $FILENAME_CERT_CSR -CA $FILENAME_CA_CERT -CAkey $FILENAME_CA_KEY -CAcreateserial -out $FILENAME_CERT_CERT -days 365 -sha256 -extfile $FILENAME_CERT_EXT
+
 # done
-echo "cert is created! please add and trust the ca.pem to your browser"
+echo "cert is created! please add and trust the $FILENAME_CA_CERT to your browser"
