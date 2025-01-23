87697265ead8e6ab6212bb7c98251ef528ed48a7
Lingjia Liu <gmutoo@gmail.com>
2025-01-23 22:37:59 +1100

    add react-ga4

--- src/analystic.ts
+++ src/analystic.ts
@@ -1,4 +1,20 @@
+import ReactGA from 'react-ga4';
+
+if (import.meta.env.VITE_GA) {
+  ReactGA.initialize(import.meta.env.VITE_GA, {
+    testMode: import.meta.env.MODE === 'development',
+  });
+}
+
 export const trackCategoryEvent =
   (category: string) => (action: string, label?: string) => {
-    console.log('track', category, action, label);
+    if (import.meta.env.VITE_GA) {
+      ReactGA.event({
+        category,
+        action,
+        label,
+      });
+    } else {
+      console.log(`track event: ${category} ${action} ${label}`);
+    }
   };
